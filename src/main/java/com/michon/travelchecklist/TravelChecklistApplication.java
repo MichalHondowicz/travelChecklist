7d85a041252ebbab43ddcbf367243f35aedf5540
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2021-11-27 22:16:56 +0100

    Initial commit

---
+++ src/main/java/com/michon/travelchecklist/TravelChecklistApplication.java
@@ -0,0 +1,13 @@
+package com.michon.travelchecklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelChecklistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelChecklistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/michon/travelchecklist/TravelChecklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michon.travelchecklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelChecklistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
